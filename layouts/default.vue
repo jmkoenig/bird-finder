d8b6835afcf7ac56c8cc605dbf547a087a0cb220
Julianne Koenig <jkoenig2017@gmail.com>
2021-10-23 17:45:49 -0400

    use default layout for common styling and flickr notice

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="c-Layout">
+    <nuxt />
+    <p>{{ flickrNotice }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator';
+
+export default class DefaultLayout extends Vue {
+  get flickrNotice () {
+      return process.env.FLICKR_NOTICE;
+  }
+}
+</script>
+
+<style lang="scss">
+.c-Layout {
+  background-color: #FFFFEF;
+}
+</style>
